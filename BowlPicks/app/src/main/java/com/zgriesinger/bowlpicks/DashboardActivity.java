23ea034a8c0dc320619ea4f6774b4d01b2ae0ad0
Zackery Griesinger <zgriesinger@gmail.com>
2017-11-05 13:44:33 -0600

    Updates to admin and UI

--- BowlPicks/app/src/main/java/com/zgriesinger/bowlpicks/DashboardActivity.java
+++ BowlPicks/app/src/main/java/com/zgriesinger/bowlpicks/DashboardActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -25,8 +29,55 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+final class League {
+    public String name;
+
+    public League(String _name) {
+        this.name = _name;
+    }
+
+    public League() {
+    }
+}
+
 public class DashboardActivity extends AppCompatActivity {
     public String league;
+
+    public void validateJoin(String leagueName) {
+        final String lname = leagueName;
+        final String err = "";
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        final DatabaseReference ref = db.getReference("Leagues");
+        final Query allLeagues = db.getReference("Leagues");
+        allLeagues.orderByChild("name").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snap : dataSnapshot.getChildren()) {
+                    League l = snap.getValue(League.class);
+                    if(lname == l.name) {
+                        err.concat("This league name is already taken!");
+                    }
+                }
+                if(err == "") {
+                    League newLeague = new League(lname);
+                    ref.child("Leagues").push().setValue(newLeague);
+                } else {
+                    Toast.makeText(getApplicationContext(), err, Toast.LENGTH_LONG).show();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void validateCreate(String leagueName) {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference allLeagues = db.getReference("Leagues");
+    }
     public void displayRow(User user){
         TableLayout pickTable = (TableLayout)findViewById(R.id.pick_table);
         TableRow row = new TableRow(this);
@@ -50,20 +101,9 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
         Toolbar dashToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(dashToolbar);
-        Button joinLeague = new Button(this);
-        joinLeague.setText("Join League");
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.FILL_PARENT);
-        params.gravity = Gravity.RIGHT;
-        joinLeague.setLayoutParams(params);
-
-        Button createLeague = new Button(this);
-        createLeague.setText("Create League");
-        createLeague.setLayoutParams(params);
-
-        dashToolbar.addView(joinLeague);
-        dashToolbar.addView(createLeague);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         Query allPicks = database.getReference("Users");
@@ -81,8 +121,76 @@ public class DashboardActivity extends AppCompatActivity {
                         leagueName.setTextSize(24);
                         leagueName.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.MATCH_PARENT));
                         LinearLayout headerLayout = (LinearLayout) findViewById(R.id.header);
-                        headerLayout.addView(leagueName);
+                        //headerLayout.addView(leagueName);
                     } else {
+                        Toolbar dashToolbar = (Toolbar) findViewById(R.id.toolbar);
+                        Button joinLeague = new Button(DashboardActivity.this);
+                        joinLeague.setText("Join League");
+                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.FILL_PARENT);
+                        params.gravity = Gravity.RIGHT;
+                        joinLeague.setLayoutParams(params);
+                        joinLeague.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View v){
+
+                                final EditText joinLeague = new EditText(DashboardActivity.this);
+
+                                LinearLayout header = (LinearLayout) findViewById(R.id.header);
+                                header.removeAllViews();
+                                joinLeague.setHint("Type League Name here:");
+                                Button submitJoin = new Button(DashboardActivity.this);
+                                submitJoin.setText("Join");
+                                submitJoin.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        String leagueName = joinLeague.getText().toString();
+                                        validateJoin(leagueName);
+                                    }
+                                });
+                                joinLeague.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.MATCH_PARENT));
+                                submitJoin.setLayoutParams(new ViewGroup.LayoutParams(
+                                        ViewGroup.LayoutParams.MATCH_PARENT,
+                                        ViewGroup.LayoutParams.WRAP_CONTENT
+                                ));
+                                header.addView(joinLeague);
+                                header.addView(submitJoin);
+
+                            }
+                        });
+
+                        Button createLeague = new Button(DashboardActivity.this);
+                        createLeague.setText("Create League");
+                        createLeague.setLayoutParams(params);
+
+                        createLeague.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View v){
+                                final EditText createLeague = new EditText(DashboardActivity.this);
+
+                                LinearLayout header = (LinearLayout) findViewById(R.id.header);
+                                header.removeAllViews();
+                                createLeague.setHint("Type League Name here to create:");
+                                Button submitCreate = new Button(DashboardActivity.this);
+                                submitCreate.setText("Join");
+                                submitCreate.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        String leagueName = createLeague.getText().toString();
+                                        validateCreate(leagueName);
+                                    }
+                                });
+                                createLeague.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.MATCH_PARENT));
+                                submitCreate.setLayoutParams(new ViewGroup.LayoutParams(
+                                        ViewGroup.LayoutParams.MATCH_PARENT,
+                                        ViewGroup.LayoutParams.WRAP_CONTENT
+                                ));
+                                header.addView(createLeague);
+                                header.addView(submitCreate);
+
+                            }
+                        });
+
+
+                        dashToolbar.addView(joinLeague);
+                        dashToolbar.addView(createLeague);
                         league = "";
                         TextView noPlayerError = new TextView(DashboardActivity.this);
                         noPlayerError.setText("Please create or join a league to see the people you are playing against!");
@@ -90,7 +198,7 @@ public class DashboardActivity extends AppCompatActivity {
                         noPlayerError.setTextSize(24);
                         noPlayerError.setTextColor(getResources().getColor(R.color.errorred));
                         LinearLayout headerLayout = (LinearLayout) findViewById(R.id.header);
-                        headerLayout.addView(noPlayerError);
+                        //headerLayout.addView(noPlayerError);
                     }
                 }
 
@@ -108,6 +216,9 @@ public class DashboardActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot snap : dataSnapshot.getChildren()) {
                     User user = snap.getValue(User.class);
+                    if(user.league == null) {
+                        user.league = "";
+                    }
                     if(user.league.equals(league)) {
                         displayRow(user);
                     }
